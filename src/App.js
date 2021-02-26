c532634de5f2a30df27fee670452d17d2c785dd5
2Large <114825758+2Large@users.noreply.github.com>
2021-02-26 11:36:30 +0000

    Rebuild website

--- src/App.js
+++ src/App.js
@@ -5,4 +5,6 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [inputText, setInputText] = useState(''
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  
