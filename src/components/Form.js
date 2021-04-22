3f56e8754b20e70c4fba2c834a7f4eab94f890f7
2Large <114825758+2Large@users.noreply.github.com>
2021-04-22 15:27:27 +0000

    Basic Structure

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,4 +36,4 @@ const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
       <div className="select">
         <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
-          
+          <option value="completed">C
