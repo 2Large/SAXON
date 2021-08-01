bcfeb595bd66ef0605bc53b2a7dba65b2ede4973
2Large <114825758+2Large@users.noreply.github.com>
2021-08-01 15:50:54 +0000

    update go

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './re
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.St
